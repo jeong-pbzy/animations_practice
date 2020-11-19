6e372f6dbeced47dd273e16d2a7f4587f0b0a107
Hannah <jeong.pbzy@gmail.com>
2020-11-19 21:33:45 +0900

    Initial commit

---
+++ app/src/main/java/com/example/anymations/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.anymations;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void fade(View view) {
+        Log.i("Info","Imageview Tapped!");
+
+        ImageView bartImageView = (ImageView) findViewById(R.id.bartImageView);
+
+        ImageView homerImageView = (ImageView) findViewById(R.id.homerImageView);
+
+        bartImageView.animate().alpha(0).setDuration(2000);
+
+        homerImageView.animate().alpha(1).setDuration(2000);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
